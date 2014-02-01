1c1cf3c11f9fa3336dabb9b6d29fc208ef684933
Heinrich Goebl <hgoebl@goebl.com>
2014-02-01 15:01:56 +0100

    add coverage report; bump version 1.1.0

--- copy-javadoc.sh
+++ copy-javadoc.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# JavaDoc
 rm -rf apidocs/
 mkdir apidocs
 cp -r ../DavidWebb/target/apidocs/ .
+
+# Coverage Report (JaCoCo)
+rm -rf jacoco/
+mkdir jacoco
+cp -r ../DavidWebb/target/site/jacoco/ .
